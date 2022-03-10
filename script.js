c79f6bc6a7849629ce3229f5a76c419534cd6a93
EjembiEmmanuel <ejembioche50@gmail.com>
2022-03-10 17:16:59 +0100

    Created game() function

--- script.js
+++ script.js
@@ -6,29 +6,53 @@ function computerPlay () {
     return choices[choice];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function play(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        computerScore++;
+        console.log("*******************************");
         return "You Lose! Paper covers Rock";
     } else if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        playerScore++;
+        console.log("*******************************");
         return "You Win! Rocks smashes Scissors";
     } else if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
+        console.log("*******************************");
         return "It's a tie";
     } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+        playerScore++;
+        console.log("*******************************");
         return "You Win! Scissors cuts Paper";
     } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'Scissors') {
+        console.log("*******************************");
         return "It's a tie";
     } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        computerScore++;
+        console.log("*******************************");
         return "You Lose! Rock smashes scissors";
     } else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
+        console.log("*******************************");
         return "It's a tie";
     } else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        playerScore++;
+        console.log("*******************************");
         return "You Win! Paper covers Rock";
     } else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        computerScore++;
+        console.log("*******************************");
         return "You Lose! Scissors cuts Paper";
     }
 }
 
-let computerChoice = computerPlay();
-let playerChoice = 'Rock';
-result = play(playerChoice, computerChoice);
-console.log(result);
+function game() {
+    let computerChoice = computerPlay();
+    let playerChoice = 'Rock';
+    let result;
+    result = play(playerChoice, computerChoice);
+    return result;
+}
+
+
+
